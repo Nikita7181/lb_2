de104ac184864258666c902b3ee63e686360699f
Nikita7181 <70635031+Nikita7181@users.noreply.github.com>
2020-10-06 18:30:29 +0300

    Update main.cpp

--- untitled/main.cpp
+++ untitled/main.cpp
@@ -47,58 +47,60 @@ y=x%5;
 
     }
         }
-cout << "task 4" << endl;
-float x,f,d;
-int l,k,n1,n2,n3,n4,n5;
+  cout << "task 4" << endl;
+    float x,f,d;
+    int l,k=0,n1=0,n2=0,n3=0,n4=0,n5=0;
     cout << "Entered value x=" << endl;
     cin >> x ;
     f=log(1+x);
     l=static_cast<int>(f*pow(10, 2))%10;
-    while (l!=k)
+    do
     {
         n1++;
         d = (pow(-1, (n1 + 1)) * ((pow(x, n1)) / n1));
-        k=static_cast<int>(d*pow(10, 2 ))%10;
-    }
-    cout << "n1= ";
-        cout << n1 << endl;
-
+        k=static_cast<int>(d*pow(10, 2))%10;
+    }while (l!=k);
+    cout << "n1=" ;
+    cout << n1 << endl;
+    k=0;d=0;
     l=static_cast<int>(f*pow(10, 3))%10;
-    while (l!=k)
+    do
     {
         n2++;
         d = (pow(-1, (n2 + 1)) * ((pow(x, n2)) / n2));
         k=static_cast<int>(d*pow(10, 3 ))%10;
-    }
-    cout << "n2= ";
+    }while (l!=k);
+    cout << "n2=";
     cout << n2 << endl;
+k=0;d=0;
     l=static_cast<int>(f*pow(10, 4))%10;
-    while (l!=k)
+    do
     {
         n3++;
         d = (pow(-1, (n3 + 1)) * ((pow(x, n3)) / n3));
         k=static_cast<int>(d*pow(10, 4 ))%10;
-    }
-    cout << "n3= ";
+    }while (l!=k);
+    cout << "n3=" ;
     cout << n3 << endl;
+    k=0;d=0;
     l=static_cast<int>(f*pow(10, 5))%10;
-    while (l!=k)
+    do
     {
         n4++;
         d = (pow(-1, (n4 + 1)) * ((pow(x, n4)) / n4));
         k=static_cast<int>(d*pow(10, 5 ))%10;
-    }
-    cout << "n4= ";
+    }while (l!=k);
+    cout << "n4=" ;
     cout << n4 << endl;
+    k=0;d=0;
     l=static_cast<int>(f*pow(10, 6))%10;
-    while (l!=k)
+    do
     {
         n5++;
         d = (pow(-1, (n5 + 1)) * ((pow(x, n5)) / n5));
         k=static_cast<int>(d*pow(10, 6 ))%10;
-    }
-    cout << "n5= ";
+    }while (l!=k);
+    cout << "n5=" ;
     cout << n5 << endl;
     return 0;
-    }
-
+}
